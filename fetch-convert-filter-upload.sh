6f4683b29c7e961a8efdf05fd3e5e418ef759045
Arnaud Dagnelies <arnaud.dagnelies@gmail.com>
2023-04-01 09:03:44 +0000

    ...

--- fetch-convert-filter-upload.sh
+++ fetch-convert-filter-upload.sh
@@ -1,11 +1,21 @@
-#/bin/bash -ex
+#/bin/bash
 
-while read line; do
-  continent=`cut -f1 line`
-  country=`cut -f2 line`
-  echo $continent $country
+set -e
+#set -x
+
+mkdir -p temp
+
+echo "---- Countries ----"
+cat countries.tsv
+
+cat countries.tsv | while read -r continent code country; do
+  echo "---- $continent $code $country ----"
   curl -o temp/$country.osm.pbf https://download.geofabrik.de/$continent/$country-latest.osm.pbf
   osmium-tool export --add-unique-id=type_id --output="temp/$country.geojsonseq" "temp/$country.osm.pbf"
   gzip -9 temp/$country.geojsonseq
   ls -lh temp
-done < countries.tsv
+done
+
+
+
+echo "---- Finished ----"
