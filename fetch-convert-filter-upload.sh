0912fa2e06d13e5adffbf1a13d264fdbbb5da4bb
Arnaud Dagnelies <arnaud.dagnelies@gmail.com>
2023-04-02 10:12:50 +0000

    ...

--- fetch-convert-filter-upload.sh
+++ fetch-convert-filter-upload.sh
@@ -10,10 +10,22 @@ cat countries.tsv
 
 cat countries.tsv | while read -r continent code country; do
   echo "---- $continent $code $country ----"
-  curl -o temp/$country.osm.pbf https://download.geofabrik.de/$continent/$country-latest.osm.pbf
-  osmium-tool export --add-unique-id=type_id --output="temp/$country.geojsonseq" "temp/$country.osm.pbf"
-  gzip -9 temp/$country.geojsonseq
-  ls -lh temp
+  #curl -o temp/$country.osm.pbf https://download.geofabrik.de/$continent/$country-latest.osm.pbf
+
+  echo "Converting PBF to GeoJson"
+  osmium export --add-unique-id=type_id --overwrite --output="temp/$country.geojsonseq" "temp/$country.osm.pbf"
+  echo "Rows: `wc -l temp/$country.geojsonseq`"
+  
+  echo 'Dropping "MultiPolygon" highways and "LineString" buildings'
+  grep -v -e '"type":"MultiPolygon".*"highway":' temp/$country.geojsonseq > temp/$country.geojsonseq.filt
+  grep -v -e '"type":"LineString".*"building":' temp/$country.geojsonseq.filt > temp/$country.geojsonseq
+  rm temp/$country.geojsonseq.filt
+  echo "Rows: `wc -l temp/$country.geojsonseq`"
+  
+  echo "Compressing..."
+  time gzip -9 -k temp/$country.geojsonseq
+  #time zstd -12 temp/$country.geojsonseq
+  ls -lh temp/$country.*
 done
 
 
