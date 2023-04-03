0e0f08b2847f3d0e2cb2adc94c147fb46820607e
Arnaud Dagnelies <arnaud.dagnelies@gmail.com>
2023-04-03 08:00:02 +0000

    ...

--- fetch-convert-filter-upload.sh
+++ fetch-convert-filter-upload.sh
@@ -13,17 +13,18 @@ cat countries.tsv | while read -r continent code country; do
   #curl -o temp/$country.osm.pbf https://download.geofabrik.de/$continent/$country-latest.osm.pbf
 
   echo "Converting PBF to GeoJson"
-  osmium export --add-unique-id=type_id --overwrite --output="temp/$country.geojsonseq" "temp/$country.osm.pbf"
+  # https://docs.osmcode.org/osmium/latest/osmium-export.html
+  osmium export --add-unique-id=type_id --overwrite --config=osmium-config.json --output="temp/$country.geo.jsonseq" "temp/$country.osm.pbf"
   echo "Rows: `wc -l temp/$country.geojsonseq`"
   
   echo 'Dropping "MultiPolygon" highways and "LineString" buildings'
-  grep -v -e '"type":"MultiPolygon".*"highway":' temp/$country.geojsonseq > temp/$country.geojsonseq.filt
-  grep -v -e '"type":"LineString".*"building":' temp/$country.geojsonseq.filt > temp/$country.geojsonseq
-  rm temp/$country.geojsonseq.filt
-  echo "Rows: `wc -l temp/$country.geojsonseq`"
+  grep -v -e '"type":"MultiPolygon".*"highway":' temp/$country.geo.jsonseq > temp/$country.geo.jsonseq.filt
+  grep -v -e '"type":"LineString".*"building":' temp/$country.geo.jsonseq.filt > temp/$country.geo.jsonseq
+  rm temp/$country.geo.jsonseq.filt
+  echo "Rows: `wc -l temp/$country.geo.jsonseq`"
   
   echo "Compressing..."
-  time gzip -9 -k temp/$country.geojsonseq
+  #time gzip -9 -k temp/$country.geojsonseq
   #time zstd -12 temp/$country.geojsonseq
   ls -lh temp/$country.*
 done
