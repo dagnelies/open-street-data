8d07e9e38e9dd15adbe195234bf31110348d662f
Arnaud Dagnelies <arnaud.dagnelies@gmail.com>
2023-04-17 08:44:02 +0200

    Update get-country-polygons.sh

--- get-country-polygons.sh
+++ get-country-polygons.sh
@@ -4,7 +4,7 @@
 #transmission-cli https://planet.openstreetmap.org/pbf/planet-latest.osm.pbf.torrent -w .
 mkdir -p borders
 
-osmium tags-filter -overwrite --output="borders/country-borders-raw.osm.pbf" planet-*.osm.pbf "wr/admin_level=2"
+osmium tags-filter --overwrite --output="borders/country-borders-raw.osm.pbf" planet-*.osm.pbf "wr/admin_level=2"
 osmium export --overwrite --output-format=geojsonseq --output="borders/country-borders-raw.geo.jsonseq" "borders/country-borders-raw.osm.pbf"
 wc -l borders/country-borders-raw.geo.jsonseq
 
