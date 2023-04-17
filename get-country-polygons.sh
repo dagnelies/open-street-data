0beb6ecf821dd0a5c5daa56608e68875612ba651
Arnaud Dagnelies <arnaud.dagnelies@gmail.com>
2023-04-17 08:42:27 +0200

    Update get-country-polygons.sh

--- get-country-polygons.sh
+++ get-country-polygons.sh
@@ -4,9 +4,9 @@
 #transmission-cli https://planet.openstreetmap.org/pbf/planet-latest.osm.pbf.torrent -w .
 mkdir -p borders
 
-osmium tags-filter -overwrite --output="temp/country-borders-raw.osm.pbf" planet-*.osm.pbf "a/admin_level=2"
-osmium export --overwrite --output-format=geojsonseq --output="temp/country-borders-raw.geo.jsonseq" "temp/country-borders-raw.osm.pbf"
-wc -l temp/country-borders-raw.geo.jsonseq
+osmium tags-filter -overwrite --output="borders/country-borders-raw.osm.pbf" planet-*.osm.pbf "wr/admin_level=2"
+osmium export --overwrite --output-format=geojsonseq --output="borders/country-borders-raw.geo.jsonseq" "borders/country-borders-raw.osm.pbf"
+wc -l borders/country-borders-raw.geo.jsonseq
 
-papermill simplify-country-borders.ipynb simplify-country-borders-run.ipynb
-jupyter nbconvert --to html simplify-country-borders-run.ipynb
+papermill simplify-country-borders.ipynb borders/simplify-country-borders-run.ipynb
+jupyter nbconvert --to html borders/simplify-country-borders-run.ipynb
